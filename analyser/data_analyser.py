69913c74f7ef7bc56ad6792f53ebbff17988357a
ZalewskiPiotr <1piotrzalewski@gmail.com>
2025-04-08 19:12:02 +0200

    Clean a text
    Added cleaning of text from special characters like '\n', '\t', ' '

--- analyser/data_analyser.py
+++ analyser/data_analyser.py
@@ -1,7 +1,18 @@
 class DataAnalyser:
 
     def __init__(self, data: list):
-        self.data_to_analyse = data
+        self.data_to_analyse: list = data
+        self.clean_data()
+
+
+    def clean_data(self):
+        for i in range(len(self.data_to_analyse)):
+            line: str = self.data_to_analyse[i]
+            line = line.replace('\n', '')
+            line = line.replace('\t', '')
+            line = line.strip()
+            self.data_to_analyse[i] = line
+
 
     def amount_of_lines(self) -> int:
         return len(self.data_to_analyse)
