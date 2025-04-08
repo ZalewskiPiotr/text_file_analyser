dc40c1b6256ea5ed089eba7b4516439194e59774
ZalewskiPiotr <1piotrzalewski@gmail.com>
2025-04-08 22:26:26 +0200

    Show top 5 frequent words
    Added a function to find the 5 most frequent words in a text file.

--- analyser/data_analyser.py
+++ analyser/data_analyser.py
@@ -1,8 +1,7 @@
 class DataAnalyser:
 
     def __init__(self, data: list):
-        self.data_to_analyse: list = data
-        self.clean_data()
+        self.data_to_analyse: list = self.clean_data(data)
 
 
     def clean_data(self, data: list) -> list:
@@ -20,5 +19,28 @@ class DataAnalyser:
     def amount_of_words(self) -> int:
         number_of_words: int = 0
         for one_line in self.data_to_analyse:
-            number_of_words += len(str(one_line).split(' '))
+            number_of_words += len(one_line.split())
         return number_of_words
+
+
+    def show_five_most_frequent_words(self) -> list:
+        counted_words: dict = self.number_of_occurrences_of_each_word(self.data_to_analyse)
+        sorted_words: list = self.sort_items(counted_words)
+        return sorted_words
+
+
+    def number_of_occurrences_of_each_word(self, source: list) -> dict:
+        counted_words: dict = {}
+        for line in source:
+            words: list = line.split()
+            for word in words:
+                if word in counted_words:
+                    counted_words[word] += 1
+                else:
+                    counted_words[word] = 1
+        return counted_words
+
+
+    def sort_items(self, data: dict) -> list:
+        sorted_words: list = sorted(data.items(), key = lambda item: item[1], reverse = True)
+        return sorted_words[:5]
