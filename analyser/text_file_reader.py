2c4b707c7e770cd69ff8130e21da3254d6b5e9db
ZalewskiPiotr <1piotrzalewski@gmail.com>
2025-04-13 23:07:07 +0200

    Added a class documentation
    Added documentation written in Google-style docstring.

--- analyser/text_file_reader.py
+++ analyser/text_file_reader.py
@@ -1,11 +1,29 @@
 class TextFileReader:
+    """
+        A simple utility class to read the contents of a text file.
+
+        Attributes:
+            file_path (str): The path to the text file.
+            file_contents (list): A list storing lines read from the file.
+        """
 
     def __init__(self, file_path: str):
+        """
+        Initializes the TextFileReader with the given file path.
+
+        Args:
+            file_path (str): Path to the text file.
+        """
         self.file_path: str = file_path
         self.file_contents: list = []
 
 
     def read_file(self):
+        """
+        Reads the content of the file line by line and stores it in file_contents.
+
+        Opens the file in read and write mode ("r+t"), reads each line and appends it to the file_contents list.
+        """
         file = open(self.file_path, "r+t")
         for line in file:
             self.file_contents.append(line)
@@ -13,4 +31,10 @@ class TextFileReader:
 
 
     def get_file_content(self):
+        """
+        Returns the contents of the file.
+
+        Returns:
+            list: A list of lines read from the file.
+        """
         return self.file_contents
